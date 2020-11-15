4337c38f447d9f42320c8ba4375fa08a224df7dc
Luis Arias <kaaloo@gmail.com>
2020-11-15 19:47:27 +0100

    feat: :sparkles: Implemented noise decay

--- REPORT.md
+++ REPORT.md
@@ -12,3 +12,25 @@ The following sections describe efforts to improve from this baseline performanc
 # Increase in Model Size
 
 A first exploratory step to improve learning is to adapt the model structure.  A cursory look at the [DDPG Bipedal course exercise](https://github.com/udacity/deep-reinforcement-learning/tree/master/ddpg-bipedal) in the Udacity Deep Reinforcement Learning repository on GitHub shows that a more complex model is needed to learn in a more complex environment.  Indeed, although the size of individual layers in the Bipedal model are individually smaller than for the Pendulum model (256 and 128 instead of 400 and 300), the Critic in the Bipedal model has a deeper architecture with three fully connected layers.
+
+Although seemingly important, this turned out to have little effect on learning.
+
+# Noise Decay
+
+On the other hand, adding appropiate noise decay to the agent produced significant results raising confidence in being able to improve learning with further tuning.  In fact, section 7 of the [DDPG article](https://arxiv.org/abs/1509.02971) provides a wealth of detail on various hyper parameters, including the 400 and 300 size fully connected layers, parameters for $\mu$, $\sigma$ and $\theta$ of 0, 0.2, and 0.15 respectively.
+
+![DDPG Article Supplementary Information](images/ddpg-article-supplementary-info.png)
+
+There was however no information on noise decay during learning in order to progressively adopt a more deterministic policy and reduce exploration.  In hindsight this makes perfect sense but I could not find a trace of this in [Shangton Zhang's Deep RL](https://github.com/ShangtongZhang/DeepRL) repo. Luckily, after some googling I stumbled upon Witold Wydmański's [DDPG Arm-Reacher repository](https://github.com/wwydmanski/DDPG-arm-reacher).  I did not however hone in on the Noise Decay issue immediately.  I instead wanted to try techniques pointed out in the DDPG Article like batch normalization but these led me nowhere.  After proceeding to a painstaking line by line comparison, and basically rewriting the training loop in the submitted notebook from scratch, I happened to notice the implementation of noise decay in [Witold's Agent implementation](https://github.com/wwydmanski/DDPG-arm-reacher/blob/master/brain/agent.py#L61).
+
+To finish up on this topic, I had after all this the idea of going back to take a peek in the section of Miguel Morales Grokking Deep Reinforcement Learning book which I acquired for the work in this Nanodegree.  Lo and behold, section 12.1.3 entitled "Exploration with deterministic policies" goes into some detail on this issue and illustrates a `NormalNoiseDecayStrategy` class which provides a mechanism for choosing between a maximum exploration and decaying noise strategy.  Too bad I hadn't fully understood the significance of this before attempting to tune learning performance.
+
+![Noise Decay Strategy](images/grokking-deep-learning-noise-decay.png)
+
+The results below are the result of a run with Noise Decay which display much better learning but which is however not on par with the performance demonstrated in Witold's notebook.
+
+# Batch Normalization
+
+Now that there was some signs that the goal of this project could be attainable, I wanted to go back to the DDPG Article and apply batch normalization since there could be sizeable differences in the range and mean of the different state parameters. Indeed in section 3 of the DDPG Article, the authors list several techniques to improve on DQN including batch normalization in the actor and critic:
+
+> In the low-dimensional case, we used batch normalization on the state input and all layers of the μ network and all layers of the Q network prior to the action input
--- ddpg_agent.py
+++ ddpg_agent.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 128        # minibatch size
+BATCH_SIZE = 512        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor
@@ -20,6 +20,7 @@ UPDATE_EVERY = 4
 
 #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 device = "cpu"
+
 
 class Agent():
     """Interacts with and learns from the environment."""
@@ -38,25 +39,33 @@ class Agent():
         self.seed = random.seed(random_seed)
 
         # Actor Network (w/ Target Network)
-        self.actor_local = Actor(state_size, action_size, random_seed).to(device)
-        self.actor_target = Actor(state_size, action_size, random_seed).to(device)
-        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
+        self.actor_local = Actor(
+            state_size, action_size, random_seed).to(device)
+        self.actor_target = Actor(
+            state_size, action_size, random_seed).to(device)
+        self.actor_optimizer = optim.Adam(
+            self.actor_local.parameters(), lr=LR_ACTOR)
 
         # Critic Network (w/ Target Network)
-        self.critic_local = Critic(state_size, action_size, random_seed).to(device)
-        self.critic_target = Critic(state_size, action_size, random_seed).to(device)
-        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+        self.critic_local = Critic(
+            state_size, action_size, random_seed).to(device)
+        self.critic_target = Critic(
+            state_size, action_size, random_seed).to(device)
+        self.critic_optimizer = optim.Adam(
+            self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
 
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        self.memory = ReplayBuffer(
+            action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
 
         # Copy initial network parameters
         self.soft_update(self.actor_local, self.actor_target, 1.0)
         self.soft_update(self.critic_local, self.critic_target, 1.0)
 
+        self.total_episodes = 1
         self.total_steps = 0
 
     def step(self, state, action, reward, next_state, done):
@@ -81,12 +90,13 @@ class Agent():
         self.actor_local.train()
 
         if add_noise:
-            action += self.noise.sample()
+            action += (self.noise.sample() * 0.6) / np.sqrt(self.total_episodes)
 
-        return np.clip(action, -1, 1)
+        return action
 
     def reset(self):
         self.noise.reset()
+        self.total_episodes += 1
 
     def learn(self, experiences, gamma):
         """Update policy and value parameters using given batch of experience tuples.
@@ -106,13 +116,17 @@ class Agent():
         # Get predicted next-state actions and Q values from target models
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
+
         # Compute Q targets for current states (y_i)
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+
         # Compute critic loss
         Q_expected = self.critic_local(states, actions)
         critic_loss = F.mse_loss(Q_expected, Q_targets)
+
         # Minimize the loss
         self.critic_optimizer.zero_grad()
+#        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1)
         critic_loss.backward()
         self.critic_optimizer.step()
 
@@ -120,6 +134,7 @@ class Agent():
         # Compute actor loss
         actions_pred = self.actor_local(states)
         actor_loss = -self.critic_local(states, actions_pred).mean()
+
         # Minimize the loss
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
@@ -140,7 +155,9 @@ class Agent():
             tau (float): interpolation parameter
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+            target_param.data.copy_(
+                tau*local_param.data + (1.0-tau)*target_param.data)
+
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
@@ -160,9 +177,11 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        dx = self.theta * (self.mu - x) + self.sigma * \
+            np.array([random.random() for i in range(len(x))])
         self.state = x + dx
         return self.state
+
 
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
@@ -175,9 +194,10 @@ class ReplayBuffer:
             batch_size (int): size of each training batch
         """
         self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.memory = ReplayMemory(max_size=buffer_size)
         self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.experience = namedtuple("Experience", field_names=[
+                                     "state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
 
     def add(self, state, action, reward, next_state, done):
@@ -187,16 +207,40 @@ class ReplayBuffer:
 
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
+        experiences = self.memory.sample(batch_size=self.batch_size)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        states = torch.from_numpy(
+            np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(
+            np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(
+            np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack(
+            [e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack(
+            [e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
 
         return (states, actions, rewards, next_states, dones)
 
     def __len__(self):
         """Return the current size of internal memory."""
-        return len(self.memory)
+        return self.memory.size
+
+# Fast ReplayMemory via https://stackoverflow.com/a/56171119
+
+
+class ReplayMemory:
+    def __init__(self, max_size):
+        self.buffer = [None] * max_size
+        self.max_size = max_size
+        self.index = 0
+        self.size = 0
+
+    def append(self, obj):
+        self.buffer[self.index] = obj
+        self.size = min(self.size + 1, self.max_size)
+        self.index = (self.index + 1) % self.max_size
+
+    def sample(self, batch_size):
+        indices = random.sample(range(self.size), batch_size)
+        return [self.buffer[index] for index in indices]
